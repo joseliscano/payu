1d9fa10f80957cdf245555f8a128d46e4968aee8
joseliscano <jose.miller.liscano@gmail.com>
2017-05-13 08:39:39 -0500

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -18,7 +18,7 @@ class ApiController extends Controller
         $response = $_REQUEST;
         if ($response['lapTransactionState'] == 'APPROVED') {
         	session()->flash('message', 'Transacción aprobada!');
-        	return view('partials.home.home', compact('response'));
+        	return view('partials.result.result', compact('response'));
         }
     	error_log("Llega index: " . print_r($_REQUEST, true) . "\n", 3, 'files/response' . Carbon::now() . '.txt');
         $products = Product::all();
