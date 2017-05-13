5fb4cabfe993e502e0c7a81cc6aca649a0748652
joseliscano <jose.miller.liscano@gmail.com>
2017-05-13 07:30:17 -0500

    test

--- resources/views/partials/forms/order.blade.php
+++ resources/views/partials/forms/order.blade.php
@@ -89,7 +89,6 @@
 			  <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
 			  <input name="buttonType" value="SIMPLE" type="hidden"/>
 			  <input name="signature" value="{{ $signature }}" type="hidden"/>
-			  <input name="test"          type="hidden"  value="1" >
 			</form>
 			
 		</div>
