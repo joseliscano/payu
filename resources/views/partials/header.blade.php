6fa8c647b91162f14e05b9948494f27242def3dc
joseliscano <jose.miller.liscano@gmail.com>
2017-05-13 08:50:44 -0500

    test

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,10 +9,10 @@
     <title>@yield('title')</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{ asset(template/css/bootstrap.css) }}" rel="stylesheet">
+    <link href="{{ asset('template/css/bootstrap.css') }}" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="{{ asset(template/css/shop-homepage.css) }}" rel="stylesheet">
+    <link href="{{ asset('template/css/shop-homepage.css') }}" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
