84416680e320b65f87b2a3de0e3ed2d23a3cafe4
joseliscano <jose.miller.liscano@gmail.com>
2017-05-15 19:57:12 -0500

    test

--- resources/views/partials/home/instructions.blade.php
+++ resources/views/partials/home/instructions.blade.php
@@ -36,14 +36,14 @@
 			<div class="col-sm-4 col-lg-4 col-md-4">
 				<h4>Instrucciones</h4>
 				<ol>
-					<li>En la pagina home, añadir cuantos productos queramos al carrito.</li>
+					<li>En la página home, añadir cuantos productos queramos al carrito.</li>
 					<li>En la página, mostrar carrito debemos generar la orden.</li>
-					<li>En la página Listar ordenes, podemos cancelar la orden, y los productos serán reembolsados</li>
-					<li>Si le damos en pagar con PayU,k nos redirigirá a la página de pagos de PayU</li>
-					<li>Cuando se rediriga a la tienda luego del pago, muestra el resultado de la operación</li>
-					<li>Cuando llegue la confirmación se actualiza el estado</li>
+					<li>En la página Listar ordenes, podemos cancelar la orden, y los productos serán reembolsados.</li>
+					<li>Si le damos en pagar con PayU, nos redirigirá a la página de pagos de PayU.</li>
+					<li>Cuando se rediriga a la tienda luego del pago, muestra el resultado de la operación.</li>
+					<li>Cuando llegue la confirmación se actualiza el estado.</li>
 					<li>Si el estado es diferente a pending, success o rejected, los productos serán reembolsados a la lista del home.</li>
-					<li>Al llegar la confirmacion, se genra un archivo que se puede consultar en el link Archivos de confirmación</li>
+					<li>Al llegar la confirmación, se genera un archivo que se puede consultar en el link Archivos de confirmación.</li>
 				</ol>
 			</div>
 		</div>
