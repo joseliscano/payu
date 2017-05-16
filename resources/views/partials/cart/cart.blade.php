3e5f6081c1826fd93716e8393e5fdb97d961ef3f
joseliscano <jose.miller.liscano@gmail.com>
2017-05-15 20:57:45 -0500

    test

--- resources/views/partials/cart/cart.blade.php
+++ resources/views/partials/cart/cart.blade.php
@@ -24,39 +24,41 @@
 			</div>
 		@endif
 		<div class="row">
-			<table class="table table-hover">
-				<thead>
-					<tr>
-						<th>Producto</th>
-						<th>Cantidad</th>
-						<th>Precio unitario</th>
-						<th>Precio total</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach($items as $item)
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<thead>
 						<tr>
-							<td>{{ $item->description }}</td>
-							<td>{{ $item->quantity }}</td>
-							<td>${{ $item->price }}</td>
-							<td>${{ $item->price * $item->quantity }}</td>
+							<th>Producto</th>
+							<th>Cantidad</th>
+							<th>Precio unitario</th>
+							<th>Precio total</th>
 						</tr>
-					@endforeach
-					<tr>
-						<td></td>
-						<td></td>
-						<th>Total a pagar</th>
-						<th>${{ $totalPrice }}</th>
-					</tr>
-				</tbody>
-			</table>
-		@if($items->count() > 0)
-		<form action="/public/order" method="post">
-		{{ csrf_field() }}
-			
-			<input type="submit" class="btn btn-primary" value="Generar orden" />
-		</form>
-		@endif
+					</thead>
+					<tbody>
+						@foreach($items as $item)
+							<tr>
+								<td>{{ $item->description }}</td>
+								<td>{{ $item->quantity }}</td>
+								<td>${{ $item->price }}</td>
+								<td>${{ $item->price * $item->quantity }}</td>
+							</tr>
+						@endforeach
+						<tr>
+							<td></td>
+							<td></td>
+							<th>Total a pagar</th>
+							<th>${{ $totalPrice }}</th>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			@if($items->count() > 0)
+			<form action="/public/order" method="post">
+			{{ csrf_field() }}
+				
+				<input type="submit" class="btn btn-primary" value="Generar orden" />
+			</form>
+			@endif
 		</div>
 	</div>
 </div>
